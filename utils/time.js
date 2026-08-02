79886e84c45d468ea37a98247c941c075b9deaac
idkjshelpofficial <yaqoob.mettss@outlook.com>
2026-08-02 12:43:16 -0400

    Update time.js

--- utils/time.js
+++ utils/time.js
@@ -1,1 +1,25 @@
 
+const config = require('../config.json');
+
+function getNow() {
+  return new Date();
+}
+
+function isSameStreakDay(lastCheckInISO) {
+  if (!lastCheckInISO) return false;
+
+  const now = new Date();
+  const last = new Date(lastCheckInISO);
+
+  const [hour, minute] = config.dayStart.split(':').map(Number);
+
+  const normalize = (d) => {
+    const nd = new Date(d);
+    nd.setHours(hour, minute, 0, 0);
+    return nd.toDateString();
+  };
+
+  return normalize(now) === normalize(last);
+}
+
+module.exports = { getNow, isSameStreakDay };
